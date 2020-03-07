b502317430d9f168145459fee9d073f44e087113
ChoungJX <zhenglinfeng43@gmail.com>
2020-03-07 22:50:42 +0800

    fix bugs

--- app/Lib/command.py
+++ app/Lib/command.py
@@ -1,31 +1,41 @@
 import subprocess
 import json
+import os
 
-def send_message(cm: str) -> subprocess.Popen:
+def send_message(cm) -> subprocess.Popen:
     thr = subprocess.Popen(
         cm, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     return thr
 
 
 def use_docker_sock(args_dict: dict) -> subprocess.Popen:
-    elementary_command = "curl -s"
+    #elementary_command = "curl -s"
+    elementary_command = ["curl","-s"]
 
     if args_dict['method'] != "GET":
-        elementary_command = "%s -X %s" % (
-            elementary_command, args_dict['method']
-        )
+        # elementary_command = "%s -X %s" % (
+        #     elementary_command, args_dict['method']
+        # )
+        elementary_command.append("-X")
+        elementary_command.append(args_dict['method'])
     else:
         pass
     if args_dict.get('data'):
-        elementary_command = '%s -H \"Content-Type: application/json\" -X POST  --data \'%s\'' % (
-            elementary_command, json.dumps(args_dict.get('data'))
-        )
+        # elementary_command = '%s -H \"Content-Type: application/json\" -X POST  --data \'%s\'' % (
+        #     elementary_command, json.dumps(args_dict.get('data'))
+        # )
+        elementary_command.append("--header")
+        elementary_command.append('"Content-Type:application/json"')
+        elementary_command.append("--data")
+        elementary_command.append("'"+json.dumps(args_dict.get('data'))+"'")
 
-    elementary_command = "%s --unix-socket /var/run/docker.sock http://localhost%s" % (
-        elementary_command, args_dict['url']
-    )
-    elementary_command = '"'.join(elementary_command.split("'"))
-    print(elementary_command)
+    # elementary_command = "%s --unix-socket /var/run/docker.sock http://localhost%s" % (
+    #     elementary_command, args_dict['url']
+    # )
+    elementary_command.append("--unix-socket")
+    elementary_command.append("/var/run/docker.sock")
+    elementary_command.append("http://localhost%s"%(args_dict['url']))
+    elementary_command=" ".join(elementary_command)
     get_thr = send_message(elementary_command)
     return get_thr
 
